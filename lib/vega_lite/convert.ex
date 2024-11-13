50f4cc6467a96efb295e9f87f8b6af48cad04d9b
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-11-13 17:20:56 +0800

    Up

--- lib/vega_lite/convert.ex
+++ lib/vega_lite/convert.ex
@@ -10,20 +10,20 @@ defmodule VegaLite.Convert do
   alias VegaLite.Convert.Native
 
   @doc """
-  Saves a `VegaLite` specification to file in one of
-  the supported formats.
+  Saves a `VegaLite` specification to a file in one of the supported
+  formats.
 
-  Any additional options provided beyond `:format` are passed to
-  the functions that export to the desired format.
+  Any additional options provided beyond `:format` are passed to the
+  functions that export to the desired format.
 
   ## Options
 
-    * `:format` - the format to export the graphic as,
-      must be either of: `:json`, `:html`, `:png`, `:svg`, `:pdf`,
-      `:jpeg`, `:jpg`. By default the format is inferred from the
-      file extension.
+    * `:format` - the format to export the graphic as, must be either
+      of: `:json`, `:html`, `:png`, `:svg`, `:pdf`, `:jpeg`, `:jpg`.
+      By default the format is inferred from the file extension.
+
   """
-  @spec save!(VegaLite.t(), binary(), keyword()) :: :ok
+  @spec save!(VegaLite.t(), Path.t(), keyword()) :: :ok
   def save!(vl, path, opts \\ []) do
     {format, opts} =
       Keyword.pop_lazy(opts, :format, fn ->
@@ -67,8 +67,8 @@ defmodule VegaLite.Convert do
   ## Options
 
     * `:target` - specifies whether JSON export is in the VegaLite
-    format or Vega. Valid options are `:vega_lite` or `:vega`. Default
-    is `:vega_lite`.
+      format or Vega. Valid options are `:vega_lite` or `:vega`.
+      Defaults to `:vega_lite`.
   """
   @spec to_json(vl :: VegaLite.t(), opts :: keyword()) :: String.t()
   def to_json(vl, opts \\ []) do
@@ -96,13 +96,14 @@ defmodule VegaLite.Convert do
 
   ## Options
 
-    * `:bundle` - Configures whether the VegaLite client side JS library is
-      embedded in the document or if it is pulled down from the CDN. Default
-      is `true`.
+    * `:bundle` - configures whether the VegaLite client side JS library
+      is embedded in the document or if it is pulled down from the CDN.
+      Defaults to `true`.
 
-    * `:renderer` - This configuration determines how the VegaLite
-      chart is rendered in the HTML document. Possible values are: `:svg`,
-      `:canvas`, or `:hybrid`. Default is `:svg`.
+    * `:renderer` - determines how the VegaLite chart is rendered in
+      the HTML document. Possible values are: `:svg`, `:canvas`, or
+      `:hybrid`. Defaults to `:svg`.
+
   """
   @spec to_html(VegaLite.t()) :: binary()
   def to_html(vl, opts \\ []) do
@@ -116,14 +117,15 @@ defmodule VegaLite.Convert do
   end
 
   @doc """
-  Renders the given graphic as a PNG image and returns
-  its binary content.
+  Renders the given graphic as a PNG image and returns its binary
+  content.
 
   ## Options
 
-    * `:scale` - The image scale factor. Default is `1.0`.
+    * `:scale` - the image scale factor. Defaults to `1.0`.
 
-    * `:ppi` - The pixels per inch. Default is `72.0`.
+    * `:ppi` - the number of pixels per inch. Defaults to `72.0`.
+
   """
   @spec to_png(VegaLite.t(), keyword()) :: binary()
   def to_png(vl, opts \\ []) do
@@ -137,8 +139,8 @@ defmodule VegaLite.Convert do
   end
 
   @doc """
-  Renders the given graphic as an SVG image and returns
-  its binary content.
+  Renders the given graphic as an SVG image and returns its binary
+  content.
   """
   @spec to_svg(VegaLite.t()) :: binary()
   def to_svg(vl) do
@@ -149,8 +151,7 @@ defmodule VegaLite.Convert do
   end
 
   @doc """
-  Renders the given graphic as a PDF and returns
-  its binary content.
+  Renders the given graphic as a PDF and returns its binary content.
   """
   @spec to_pdf(VegaLite.t()) :: binary()
   def to_pdf(vl) do
@@ -161,15 +162,16 @@ defmodule VegaLite.Convert do
   end
 
   @doc """
-  Renders the given graphic as a JPEG image and returns
-  its binary content.
+  Renders the given graphic as a JPEG image and returns its binary
+  content.
 
   ## Options
 
-    * `:scale` - The image scale factor. Default is `1.0`.
+    * `:scale` - the image scale factor. Defaults to `1.0`.
 
-    * `:quality` - The quality of the generated JPEG between 0
-      (worst) and 100 (best). Default is `90`.
+    * `:quality` - the quality of the generated JPEG between 0 (worst)
+      and 100 (best). Defaults to `90`.
+
   """
   @spec to_jpeg(VegaLite.t(), keyword()) :: binary()
   def to_jpeg(vl, opts \\ []) do
@@ -183,9 +185,9 @@ defmodule VegaLite.Convert do
   end
 
   @doc """
-  Renders a `VegaLite` specification in GUI window widget.
+  Renders a `VegaLite` graphic in a GUI window widget.
 
-  Requires Erlang compilation to include the `:wx` module.
+  This requires the Erlang compilation to include the `:wx` module.
   """
   @spec show(VegaLite.t()) :: :ok | :error
   def show(vl) do
