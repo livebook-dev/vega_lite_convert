e74851a3b0a780c87e978c18c5c23be11ca8f6d0
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-01-30 01:35:22 +0700

    Fix segmentation fault on x86

--- native/ex_vl_convert/src/lib.rs
+++ native/ex_vl_convert/src/lib.rs
@@ -2,11 +2,20 @@ use rustler::env::Env;
 use rustler::types::binary::Binary;
 use rustler::types::binary::OwnedBinary;
 
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 use vl_convert_rs::anyhow::Error;
 use vl_convert_rs::converter::VgOpts;
 use vl_convert_rs::converter::VlOpts;
 use vl_convert_rs::VlConverter;
 use vl_convert_rs::VlVersion;
+
+// We use a single instance of VlConvert to avoid segmentation faults.
+// Note that this limits us to a single concurrent conversion at a time.
+// See https://github.com/vega/vl-convert/issues/206#issuecomment-2598336507
+lazy_static! {
+    static ref VL_CONVERTER: Mutex<VlConverter> = Mutex::new(VlConverter::new());
+}
 
 // +-------------------------------------+
 // |            Vega Functions           |
@@ -16,7 +25,10 @@ use vl_convert_rs::VlVersion;
 fn vega_to_svg(vega_spec: String) -> Result<String, String> {
     let vg_spec = parse_spec(vega_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let svg_result = futures::executor::block_on(converter.vega_to_svg(vg_spec, vg_opts()));
 
     return encode_string_result(svg_result);
@@ -31,7 +43,10 @@ fn vega_to_html(vega_spec: String, bundle: bool, renderer: String) -> Result<Str
         Err(_err) => return Err("Invalid renderer provided".to_string()),
     };
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let html_result = futures::executor::block_on(converter.vega_to_html(
         vg_spec,
         vg_opts(),
@@ -46,7 +61,10 @@ fn vega_to_html(vega_spec: String, bundle: bool, renderer: String) -> Result<Str
 fn vega_to_png(env: Env, vega_spec: String, scale: f32, ppi: f32) -> Result<Binary, String> {
     let vg_spec = parse_spec(vega_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let png_result = futures::executor::block_on(converter.vega_to_png(
         vg_spec,
         vg_opts(),
@@ -61,7 +79,10 @@ fn vega_to_png(env: Env, vega_spec: String, scale: f32, ppi: f32) -> Result<Bina
 fn vega_to_jpeg(env: Env, vega_spec: String, scale: f32, quality: u8) -> Result<Binary, String> {
     let vg_spec = parse_spec(vega_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let jpeg_result = futures::executor::block_on(converter.vega_to_jpeg(
         vg_spec,
         vg_opts(),
@@ -76,7 +97,10 @@ fn vega_to_jpeg(env: Env, vega_spec: String, scale: f32, quality: u8) -> Result<
 fn vega_to_pdf(env: Env, vega_spec: String) -> Result<Binary, String> {
     let vg_spec = parse_spec(vega_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let pdf_result = futures::executor::block_on(converter.vega_to_pdf(vg_spec, vg_opts()));
 
     return encode_vec_result(env, pdf_result);
@@ -90,7 +114,10 @@ fn vega_to_pdf(env: Env, vega_spec: String) -> Result<Binary, String> {
 fn vegalite_to_svg(vega_lite_spec: String) -> Result<String, String> {
     let vl_spec = parse_spec(vega_lite_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let svg_result = futures::executor::block_on(converter.vegalite_to_svg(vl_spec, vl_opts()));
 
     return encode_string_result(svg_result);
@@ -109,7 +136,10 @@ fn vegalite_to_html(
         Err(_err) => return Err("Invalid renderer provided".to_string()),
     };
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let html_result = futures::executor::block_on(converter.vegalite_to_html(
         vl_spec,
         vl_opts(),
@@ -129,7 +159,10 @@ fn vegalite_to_png(
 ) -> Result<Binary, String> {
     let vl_spec = parse_spec(vega_lite_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let png_result = futures::executor::block_on(converter.vegalite_to_png(
         vl_spec,
         vl_opts(),
@@ -149,7 +182,10 @@ fn vegalite_to_jpeg(
 ) -> Result<Binary, String> {
     let vl_spec = parse_spec(vega_lite_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let jpeg_result = futures::executor::block_on(converter.vegalite_to_jpeg(
         vl_spec,
         vl_opts(),
@@ -164,7 +200,10 @@ fn vegalite_to_jpeg(
 fn vegalite_to_pdf(env: Env, vega_lite_spec: String) -> Result<Binary, String> {
     let vl_spec = parse_spec(vega_lite_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let pdf_result = futures::executor::block_on(converter.vegalite_to_pdf(vl_spec, vl_opts()));
 
     return encode_vec_result(env, pdf_result);
@@ -174,7 +213,10 @@ fn vegalite_to_pdf(env: Env, vega_lite_spec: String) -> Result<Binary, String> {
 fn vegalite_to_vega(vega_lite_spec: String) -> Result<String, String> {
     let vl_spec = parse_spec(vega_lite_spec)?;
 
-    let mut converter = VlConverter::new();
+    let mut converter = VL_CONVERTER
+        .lock()
+        .expect("Failed to acquire lock on Vega-Lite converter");
+
     let result = futures::executor::block_on(converter.vegalite_to_vega(vl_spec, vl_opts()));
 
     return match result {
